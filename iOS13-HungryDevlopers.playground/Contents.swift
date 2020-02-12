45503c45b73157b20d4dde8750e1d91620f07fc6
AustinPotts <pottsevolvements@gmail.com>
2020-02-12 18:54:19 -0500

    Completed steps 1-6

--- iOS13-HungryDevlopers.playground/Contents.swift
+++ iOS13-HungryDevlopers.playground/Contents.swift
@@ -1,9 +1,11 @@
 import UIKit
 
+// Create a class called Spoon. It should have two methods, pickUp() and putDown().
 class Spoon {
     
     let lock = NSLock()
     
+    // You can implement this with a private lock property
     func pickUp(){
         lock.lock()
     }
@@ -14,13 +16,35 @@ class Spoon {
     
 }
 
-
+//Create a class called Developer. Each Developer should have a leftSpoon property and a rightSpoon property. It should also have think(), eat(), and run() methods.
 class Developer {
+    
+    let lock = NSLock()
+    
     var leftSpoon: Spoon
     var rightSpoon: Spoon
     
     init(leftSpoon: Spoon, rightSpoon: Spoon){
         self.leftSpoon = leftSpoon
         self.rightSpoon = rightSpoon
+    }
+    
+    //think() should pick up both spoons before returning.
+    func think(){
+         leftSpoon
+         rightSpoon
+    }
+    
+    //eat() should pause for a random amount of time before putting both spoons down.
+    func eat(){
+        usleep(1)
+        rightSpoon.putDown()
+        leftSpoon.putDown()
+    }
+    
+    //Developer.run() should call think() then eat() over and over again forever.
+    func run(){
+        think()
+        eat()
     }
 }
